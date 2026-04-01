6fcb2bc41a22289a8318eb8d5ac7e956030947ef
mohamedrrabie <53351776+mohamedrrabie@users.noreply.github.com>
2026-04-01 16:22:49 +0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@ const CACHE_NAME = 'exam-analyzer-v2';
 const urlsToCache = [
   './',
   './index.html',
-  './Exam Analyzer.html',
+  './ExamAnalyzer.html',
   './manifest.json',
   './icon-192x192.png',
   './icon-512x512.png'
